812b301e634cb1d225d312a8794addf9c3f2f8df
Nathan Foxley <nfoxley@sgifs.com>
2016-08-22 15:43:21 -0500

    Gulp progress

--- src/BoxMetaphorSite/gulpfile.js
+++ src/BoxMetaphorSite/gulpfile.js
@@ -1,4 +1,24 @@
-var sass = require("gulp-sass");
+/// <binding BeforeBuild='clean, min, sass' />
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify");
+
+var project = require("./project.json");
+
+var paths = {
+    webroot: "./wwwroot/"
+};
+
+paths.js = paths.webroot + "js/**/*.js";
+paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatCssDest = paths.webroot + "css/site.min.css";
+
 // where to find sass code
 paths.sassSource = "./Scss/*.scss";
 
@@ -8,13 +28,39 @@ paths.cssOutput = paths.webroot + "css";
 // where to find bower resources
 paths.bower_components = paths.webroot + "lib/";
 
-gulp.task('sass', function () {
+gulp.task("clean:js", function (cb) {
+    rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+    rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css"]);
+
+gulp.task("min:js", function () {
+    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+      .pipe(concat(paths.concatJsDest))
+      .pipe(uglify())
+      .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function () {
+    return gulp.src([paths.css, "!" + paths.minCss])
+      .pipe(concat(paths.concatCssDest))
+      .pipe(cssmin())
+      .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("sass", function () {
     gulp.src(paths.sassSource)
         .pipe(sass({
             includePaths: [
-                paths.bower_components + 'foundation-sites/scss',
-                paths.bower_components + 'motion-ui/src'
+                paths.bower_components + "foundation-sites/scss",
+                paths.bower_components + "motion-ui/src"
             ]
-        }).on('error', sass.logError))
+        }).on("error", sass.logError))
         .pipe(gulp.dest(paths.cssOutput));
 });
